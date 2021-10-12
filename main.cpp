22618702970b2affad72fbaf308438943ec36475
Francesca Minardi <fminardi@e4r9p4.42roma.it>
2021-10-12 18:33:36 +0200

    facciamo schifo

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "vector.hpp"
+
+int main()
+{
+	ft::vector<int> ciccio;
+	std::vector<int> gamer;
+	std::cout << gamer.size() << std::endl;
+	std::cout << ciccio.size();
+
+
+}
--- vector.hpp
+++ vector.hpp
@@ -10,39 +10,71 @@
 #include <memory>
 #include <string>
 #include <utility>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <string>
+#include <utility>
+#include <iterator>
+#include <vector>
+  
 
-namespace ft
+
+
+namespace ft 
 {
-			typedef typename allocator_type::pointer	pointer;
-			typedef	T											value_type;
-			typedef	Alloc										allocator_type;
-			typedef	typename allocator_type::reference			reference;
-			typedef typename allocator_type::const_reference	const_reference;
-			typedef typename allocator_type::pointer			pointer;
-			typedef typename allocator_type::const_pointer		const_pointer;
-			typedef	vector_iterator<pointer>					iterator;
-			typedef	vector_iterator<const_pointer>				const_iterator;
-			typedef ft::reverse_iterator<iterator>				reverse_iterator;
-			typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
-			typedef std::ptrdiff_t								difference_type;
-			typedef size_t										size_type;
-	template <class T, class Alloc = std::allocator<T>>
-	class vector
-	{
-		private:
-			size_t _size;
-			size_t _capacity;
-			pointer _vector;
-			Alloc _alloc;
-			std::iterator it;
-		public:
-/*default*/ vector(const allocator_type& alloc = allocator_type()))
-			{
-				
-			}
-/*fill*/	explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
-/*range*/	template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
-/*copy*/	vector (const vector& x);
+  template < class T, class Allocator = std::allocator<T> > 
+  class vector 
+  {
+  	public:
+   	 	// types
+		typedef T                                        	value_type;
+		typedef Allocator                                	allocator_type;
+		typedef typename allocator_type::reference       	reference;
+		typedef typename allocator_type::const_reference 	const_reference;
+		typedef typename allocator_type::pointer         	pointer;
+		typedef typename allocator_type::const_pointer  	const_pointer;
+		typedef typename allocator_type::size_type       	size_type;
+		//typedef vector_iterator<pointer>					iterator;
+		//typedef vector_iterator <const_pointer>				const_iterator;
+		//typedef std::reverse_iterator<iterator>    			const_reverse_iterator;
+		typedef typename allocator_type::difference_type 	difference_type;
+		//typedef std::reverse_iterator<iterator>         	reverse_iterator;
+		size_t size() { return (_size);}
+		size_t capacity() {return (_capacity);}
+		bool empty() const {
+			if (_capacity == 0)
+				return (true);
+			return(false);
+		}
+		void reserve( size_type new_cap ){
+			_capacity = new_cap; 
+			//If new_cap is greater than capacity(), all iterators, including the past-the-end iterator, 
+			//and all references to the elements are invalidated. Otherwise, no iterators or references are invalidated.
+		}
+		size_type max_size() const{ return (std::distance(begin(), end())); }
+		explicit vector(const allocator_type& alloc = allocator_type()):  _vector(nullptr), _size(0), _capacity(0){};
+		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+		{
+
+		}
+		template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+		{
+
+		}
+		vector (const vector& x)
+		{
+
+		}
+
+	private:
+		size_t _size;
+		size_t _capacity;
+		pointer _vector;
+		Allocator _alloc;
+		//iterator it;
+
 	};
 
 
