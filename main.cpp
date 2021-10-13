5edb393131715caee54369e146c2e89ba31642b5
Francesca Minardi <fminardi@e4r10p4.42roma.it>
2021-10-13 17:54:00 +0200

    forse qualcosa

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "vector.hpp"
+#include <vector>
+#include <iostream>
+
 
 int main()
 {
-	ft::vector<int> ciccio;
-	std::vector<int> gamer;
-	std::cout << gamer.size() << std::endl;
-	std::cout << ciccio.size();
+	ft::vector<int> ciccio(5, 6);
+	std::vector<int> gamer (5, 6);
+
+	for (int i = 0; i < 6; i++)
+		std::cout << ciccio[i] << std::endl;
+	
 
 
 }
--- vector.hpp
+++ vector.hpp
@@ -41,28 +41,72 @@ namespace ft
 		//typedef std::reverse_iterator<iterator>    			const_reverse_iterator;
 		typedef typename allocator_type::difference_type 	difference_type;
 		//typedef std::reverse_iterator<iterator>         	reverse_iterator;
-		size_t size() { return (_size);}
-		size_t capacity() {return (_capacity);}
-		bool empty() const {
-			if (_capacity == 0)
-				return (true);
-			return(false);
-		}
-		void reserve( size_type new_cap ){
-			_capacity = new_cap; 
-			//If new_cap is greater than capacity(), all iterators, including the past-the-end iterator, 
-			//and all references to the elements are invalidated. Otherwise, no iterators or references are invalidated.
-		}
-		size_type max_size() const{ return (std::distance(begin(), end())); }
-		explicit vector(const allocator_type& alloc = allocator_type()):  _vector(nullptr), _size(0), _capacity(0){};
-		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
-		{
 
-		}
-		template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
-		{
+		const_reference operator[]( size_type pos ) const { return (this->_begin[pos]); }
+		reference operator[]( size_type pos )  { return (this->_begin[pos]); }
 
+		size_t size() { return (_size); }
+		size_t capacity() { return (_capacity); }
+		bool empty() const { return(_capacity == 0); }
+		// vector<_Tp, _Allocator>::resize(size_type __sz)
+		// {
+		// 	size_type __cs = size();
+		// 	if (__cs < __sz) // size < nuova capacity
+		// 		this->__append(__sz - __cs);
+		// 	else if (__cs > __sz)
+		// 		this->__destruct_at_end(this->__begin_ + __sz);
+		// }
+
+		//template <class _Tp, class _Allocator>
+		// void vector<_Tp, _Allocator>::reserve(size_type __n)
+		// {
+		// 	if (__n > capacity())
+		// 	{
+		// 		allocator_type& __a = this->__alloc();
+		// 		__split_buffer<value_type, allocator_type&> __v(__n, size(), __a);
+		// 		__swap_out_circular_buffer(__v);
+		// 	}
+		// }
+		// void reserve( size_type new_cap )
+		// {
+		// 	if (new_cap > _capacity)
+		// 	{
+		// 		_capacity = new_cap;
+
+		// 	}
+	
+		// 	//If new_cap is greater than capacity(), all iterators, including the past-the-end iterator, 
+		// 	//and all references to the elements are invalidated. Otherwise, no iterators or references are invalidated.
+		// }
+		//size_type max_size() const{ return (std::distance(begin(), end())); }
+		
+		// COSTRUTTORIIIIIIIIIII
+		explicit vector(const allocator_type& alloc = allocator_type())
+			:	_size(0),
+				_capacity(0),
+				_begin(nullptr),
+				_end(nullptr),
+				_alloc(alloc)
+		{
 		}
+		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): _size(n), _alloc(alloc)
+		{
+			allocator_type cicc;
+			Allocator cc;
+			
+			if (n > 0)
+			{
+				_capacity = _size*2;
+				_begin = _alloc.allocate(_capacity);
+				for(size_t i = 0; i < n; i++)
+					_begin[i] = val;
+			}
+	
+		}
+		// template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
+		// {
+
+		// }
 		vector (const vector& x)
 		{
 
@@ -71,9 +115,9 @@ namespace ft
 	private:
 		size_t _size;
 		size_t _capacity;
-		pointer _vector;
+		pointer _begin;
+		pointer _end;
 		Allocator _alloc;
-		//iterator it;
 
 	};
 
