1d5984eec04ddc195a65f78b0f5d45a5b2eb61e4
minardi_f@libero.it <fminardi@DESKTOP-S01D5NI.localdomain>
2021-10-17 19:04:26 +0200

    vedi iterator ciao luce splendente

--- iterator.hpp
+++ iterator.hpp
@@ -3,8 +3,21 @@
 
 #include <cstddef>
 #include <iostream>
+#include <iterator>
 
 namespace ft
 {
+public:
+	template< class Category, class T,  class Distance = std::ptrdiff_t, class Pointer = T*, class Reference = T& > 
+	struct iterator;
+	template <class I>
+	  struct iterator_traits {
+    typedef Category	iterator_category ;
+    typedef T			value_type        ;
+    typedef Distance	difference_type   ;
+    typedef T*			pointer           ;
+    typedef T&			reference         ;
+
+
 }
 #endif
